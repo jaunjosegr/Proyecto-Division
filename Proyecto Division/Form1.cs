534af8f40620b2ee7e134eb700105eba671258c3
jg602 <jg602@JUANJOS>
2022-09-05 09:09:40 -0700

    Agregar archivos de proyecto.

---
+++ Proyecto Division/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Division
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cldivision ss = new cldivision();
+
+            ss.N1 = double.Parse(textBox1.Text);
+            ss.N2 = double.Parse(textBox2.Text);
+
+            textBox3.Text = ss.DVSN().ToString();
+
+            // aqui se crea el objeto ODVSN
+            cldivision ODVSN;
+
+            // aqui manda los parametros(valores) al construct
+            ODVSN = new cldivision(double.Parse(textBox1.Text), 
+            double.Parse(textBox2.Text)); textBox3.Text = ODVSN.DVSN().
+            ToString();
+        }
+    }
+}
---
+++ Proyecto Division/cldivision.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Division
+{
+    internal class cldivision
+    {
+        private double n1;
+        private double n2;
+
+        public cldivision(double n1, double n2)
+        {
+            this.n1 = n1;
+            this.n2 = n2;
+        }
+
+       
+        public double N1 { get => n1; set => n1 = value; }
+        public double N2 { get => n2; set => n2 = value; }
+
+        public double DVSN()
+        {
+            return n1 / n2;
+        }
+
+        public cldivision()
+        {
+
+        }
+
+    }
+
+}
